4a6bd5a3a1ce0cb5194952ef5dd9ce658a217585
Luong Quoc Dinh <luongquocdinh.95@gmail.com>
2019-02-27 22:20:25 +0700

    install library laravel/socialite

--- config/services.php
+++ config/services.php
@@ -44,4 +44,9 @@ return [
         ],
     ],
 
+    'facebook' => [
+        'client_id' => env ( 'FB_CLIENT_ID' ),
+        'client_secret' => env ( 'FB_CLIENT_SECRET' ),
+        'redirect' => env ( 'FB_REDIRECT' )
+    ],
 ];
